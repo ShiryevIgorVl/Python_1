12cc00278c814beca3cbb1c20506a975712d5163
igo11 <ishirayev@gmail.com>
2025-02-06 14:36:08 +0300

    Выполнен анализ символов

--- WarAndPeace.py
+++ WarAndPeace.py
@@ -5,11 +5,11 @@ wap_file_name = "war_and_peace_UTF-8.txt"
 
 wap_processed_file_name = "wap_processed_file_name.txt"
 
-statistics_dictionary = {}
+statistics_dictionary = {".": "", ",": "", "?": "", "!": ""}
 
 
 # Читаем файл war_and_peace.txt и возвращаем список char
-def read_txt_file_to_list(file_name):
+def read_txt_file_to_list(file_name) -> list | Any:
     file = open(file_name, "r", encoding="utf-8", errors="replace")
     wap_list = list(file.read())
     file.close()
@@ -17,7 +17,7 @@ def read_txt_file_to_list(file_name):
 
 
 # Удаляем разные символы из списка и преобразум список в строку
-# def removing_unreadable_characters(list_for_removing_spaces):
+# def removing_unreadable_characters(list_for_removing_spaces) -> list | Any:
 #     my_list = []
 #     for i in range(len(list_for_removing_spaces) - 1):
 #         if (list_for_removing_spaces[i] != " ") and (list_for_removing_spaces[i] != "\xa0") and (
@@ -27,8 +27,8 @@ def read_txt_file_to_list(file_name):
 #     # string = "".join(map(str, my_list))
 #     return my_list
 
-# Удаляем разные символы из списка и преобразум список в строку
-def removing_unreadable_characters(list_for_removing_spaces):
+# Удаляем разные символы из списка
+def removing_unreadable_characters(list_for_removing_spaces) -> list | Any:
     my_list = []
     for i in range(len(list_for_removing_spaces) - 1):
         if (list_for_removing_spaces[i] != " ") and (list_for_removing_spaces[i] != "\xa0") and (
@@ -48,7 +48,10 @@ def write_file(file_name, sting_for_write):
 
 
 # Вставляем пробел после завершения предложения
-def insert_space_after_dot(input_list):
+def insert_space_after_dot(input_list) -> str | Any:
+    """
+    :type input_list: list
+    """
     for i in range(len(input_list) - 1):
         if (input_list[i] == ".") or (input_list[i] == "!") or (input_list[i] == "?") or (input_list[i] == "."):
             input_list.insert(i + 1, " ")
@@ -56,40 +59,66 @@ def insert_space_after_dot(input_list):
     return string
 
 
-# Делим String в List по предложениям, разделитеь пробел
-def devision_to_parts(string_for_devision) -> list | Any:
-    list_output = string_for_devision.split(" ")
+# Делим String в List по предложениям, разделитель пробел
+def devision_to_parts(string_for_division) -> list | Any:
+    """
+    :type string_for_division: str
+    """
+    list_output = string_for_division.split(" ")
     return list_output
 
 
+# Заполняем словарь ключами из алфавитных char
 def filling_dictionary_abc_chars(dictionary) -> dict | Any:
     for i in range(ord('а'), ord('я') + 1):
         dictionary[chr(i)] = ""
     return dictionary
 
 
-def statistics_char(list_char, input_char) -> int | Any:
+# Считаем статистику по буквам в тексте
+def statistics_char(list_char, input_char) -> float | Any:
     """
     :type input_char: str
     :type list_char: list
     """
+    len_list_char = len(list_char)
     count = 0
-
     for j in range(0, len(list_char) - 1):
-        if list_char[j] == input_char:
+        if "".join(map(str, list_char[j])).lower() == input_char.lower():
             count += 1
-    return count
+    statistic = float(count / len_list_char)
+    return "{:.3f}".format(statistic)
 
 
-filling_dictionary_abc_chars(statistics_dictionary)
+# Заполняем словарь с ключами из алфавитных char значениями отношений букв к общему количеству букв в файле
+def filling_dictionary_values(dictionary_input, list_input) -> dict | Any:
+    """
+    :type list_input: list
+    :type dictionary_input: dict
+    """
+    for key in dictionary_input:
+        relation = statistics_char(list_input, key)
+        dictionary_input[key] = relation
+    return dictionary_input
 
+
+# Выводим словарь в консоль
+def dictionary_abc_print(dictionary_for_print):
+    """
+    :type dictionary_for_print: dict
+    """
+    for key, values in dictionary_for_print.items():
+        print(key + ":", values)
+
+
+dictionary_abc_chars = filling_dictionary_abc_chars(statistics_dictionary)
 war_and_peace_list = read_txt_file_to_list(wap_file_name)
-
 list_for_write_file = removing_unreadable_characters(war_and_peace_list)
-counter_char = statistics_char(list_for_write_file, 'а')
+dictionary_abc = filling_dictionary_values(dictionary_abc_chars, list_for_write_file)
+dictionary_abc_print(dictionary_abc)
 
 # string_for_write_file = insert_space_after_dot(list_for_write_file)
 # list_devision = devision_to_parts(string_for_write_file)
-# write_file(wap_processed_file_name, string_for_write_file)
 
-print("Счетчик Я", counter_char)
+
+# write_file(wap_processed_file_name, string_for_write_file)
